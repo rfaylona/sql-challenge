024648d1799e057d24c0035e4446131197d3a57d
Ramon Faylona <rfaylona@Ramons-MacBook-Pro.local>
2021-12-06 15:12:56 -0800

    adding latest progression

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -72,3 +72,11 @@ REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
